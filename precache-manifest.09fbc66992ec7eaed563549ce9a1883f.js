0b8ea2410b58f11539476ce2805c6efa983469cb
Niv Zelber <nivznivz0@gmail.com>
2020-07-11 17:53:20 +0300

    Updates

---
+++ precache-manifest.09fbc66992ec7eaed563549ce9a1883f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb0ac3a329d2d510986007d86b028b7",
+    "url": "/react-redux-new/index.html"
+  },
+  {
+    "revision": "1aebc6d871930352701e",
+    "url": "/react-redux-new/static/js/2.3eba7e78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-new/static/js/2.3eba7e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8581f0db8943e919f6a0",
+    "url": "/react-redux-new/static/js/main.754017a5.chunk.js"
+  },
+  {
+    "revision": "215ec6f77c4c3f841d24",
+    "url": "/react-redux-new/static/js/runtime-main.fd3ac897.js"
+  }
+]);
